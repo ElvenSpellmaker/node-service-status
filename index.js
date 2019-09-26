c08f7ccfc77d1a87d3cdd1cdff83e6b7eb1b1b02
Jack Blower <Jack@elvenspellmaker.co.uk>
2019-09-27 00:04:00 +0100

    Adds forgotten logger to the index
      * Bumps the package version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const Logger = require('./src/Logger.js');
 const ServiceStatus = require('./src/ServiceStatus.js');
 const ServiceStatusMonitor = require('./src/ServiceStatusMonitor.js');
 
 module.exports = {
+	Logger,
 	ServiceStatus,
 	ServiceStatusMonitor,
 };
